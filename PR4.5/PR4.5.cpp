87a2abe3cad3af0d70f27c41e747cd553bf39d15
Volodymyr Vysotskyi <vova.vysot2.0@gmail.com>
2022-10-27 12:06:32 +0300

    1th commit master

---
+++ PR4.5/PR4.5.cpp
@@ -0,0 +1,50 @@
+﻿// Lab_04_5.cpp 
+// < Висоцький Володимир > 
+// Лабораторна робота № 4.5 
+// «Попадання» у плоску фігуру
+// Варіант 1
+#include <iostream>
+#include <iomanip>
+#include <time.h>
+
+using namespace std;
+
+int main()
+{
+	double x;
+	double y;
+	double R;
+	cout << "R = "; cin >> R;
+	//  1) спосіб
+	for (int i = 0; i <= 10; i++) {
+		cout << "x = "; cin >> x;
+		cout << "y = "; cin >> y;
+		if ((y * y <= R * R - x * x && y >= x && x >= 0) ||
+			(y * y <= R * R - x * x && y <= x && x <= 0)) {
+			cout << "yes" << endl;
+		}
+		else {
+			cout << "no" << endl;
+		}
+	}
+
+	//2) спосіб
+	cout << fixed;
+	srand((unsigned)time(NULL));
+
+	for (int i = 0; i <= 10; i++) {
+		x = 2 * R * rand() / RAND_MAX - R;
+		y = 2 * R * rand() / RAND_MAX - R;
+		if ((y * y <= R * R - x * x && y >= x && x >= 0) ||
+			(y * y <= R * R - x * x && y <= x && x <= 0)) {
+			cout << setw(8) << setprecision(4) << x << "  "
+				<< setw(8) << setprecision(4) << y << "  " << "yes" << endl;
+		}
+		else {
+			cout << setw(8) << setprecision(4) << x << "  "
+				<< setw(8) << setprecision(4) << y << "  " << "no" << endl;
+		}
+	}
+
+	return 0;
+}
